cbde9a40a6f58bc57f8d63e93d331adf54d95d80
Mr. Web Developer <117938690+Yahyobek-Coder@users.noreply.github.com>
2022-12-30 20:11:19 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+        function toggleMenu(){
+
+            const menuToggle = document.querySelector('.toggle');
+
+            const sidebar = document.querySelector('.sidebar');
+
+            menuToggle.classList.toggle('active');
+            
+            sidebar.classList.toggle('active');
+        }
